9aad71728c19b7181c81d07ce571c65da7b8d966
Tanh21124 <126349205+Tanh21124@users.noreply.github.com>
2024-01-14 19:38:26 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function myFunction() {
   var x = document.getElementById("nav2Links");
   if (x.style.display === "none") {
-    x.style.display = "block"; x.style.backgroundColor = "#FFDD00"; x.style.padding = "3px";
+    x.style.display = "block"; x.style.backgroundColor = "#FBECB2"; x.style.padding = "3px";
   } else {
     x.style.display = "none";
   }
